b9ac0ecdf27d8af1540fa6f365599cc4fa8cd3fd
Ademola Abdullahi <130805693+Ademola90@users.noreply.github.com>
2025-09-29 06:06:16 +0100

    tmk

--- src/pages/PropertyDetail.tsx
+++ src/pages/PropertyDetail.tsx
@@ -152,7 +152,7 @@ const PropertyDetail = () => {
         </div>
       </div>
 
-      <div className="container mx-auto px-4 py-8 px-5 md:px-10 lg:px-16">
+      <div className="container mx-auto py-8 px-5 md:px-10 lg:px-16">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           {/* Main Content */}
           <div className="lg:col-span-2 space-y-8">
