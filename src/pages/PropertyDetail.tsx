6a2f5e8de5fdd53daa331470a0c983c6713f21de
Ademola Abdullahi <130805693+Ademola90@users.noreply.github.com>
2025-09-29 23:10:19 +0100

    tmk

--- src/pages/PropertyDetail.tsx
+++ src/pages/PropertyDetail.tsx
@@ -114,9 +114,9 @@ const PropertyDetail = () => {
   }
 
   const formatCurrency = (amount: number) => {
-    return new Intl.NumberFormat("en-US", {
+    return new Intl.NumberFormat("en-NG", {
       style: "currency",
-      currency: "USD",
+      currency: "NGN",
       minimumFractionDigits: 0,
     }).format(amount);
   };
